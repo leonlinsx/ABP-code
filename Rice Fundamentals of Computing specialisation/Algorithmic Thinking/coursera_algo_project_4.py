a5a1d09820a0cbaa900de077e1887beb13caa59b
leonlinsx <46635657+leonlinsx@users.noreply.github.com>
2024-08-07 21:23:10 -0500

    refactor compute alignment matrix for global flag to max score if/else

--- Rice Fundamentals of Computing specialisation/Algorithmic Thinking/coursera_algo_project_4.py
+++ Rice Fundamentals of Computing specialisation/Algorithmic Thinking/coursera_algo_project_4.py
@@ -60,47 +60,25 @@ def compute_alignment_matrix(seq_x, seq_y, scoring_matrix, global_flag):
     num_rows = len(seq_x) + 1
     num_cols = len(seq_y) + 1
 
-    if global_flag:
-        # fill in first row and first col where matching with increasing number of '-'
-        for row_idx in range(1, num_rows):
-            score = alignment_matrix[row_idx - 1][0] + scoring_matrix[seq_x[row_idx - 1]]['-']
-            alignment_matrix.insert(row_idx, [score])
+    # fill in first row and first col where matching with increasing number of '-'
+    for row_idx in range(1, num_rows):
+        score = alignment_matrix[row_idx - 1][0] + scoring_matrix[seq_x[row_idx - 1]]['-']
+        alignment_matrix.insert(row_idx, [max(score, 0) if not global_flag else score])
+    for col_idx in range(1, num_cols):
+        score = alignment_matrix[0][col_idx - 1] + scoring_matrix['-'][seq_y[col_idx - 1]]
+        alignment_matrix[0].insert(col_idx, max(score, 0) if not global_flag else score)
+
+    # dynamic programming fill in rest of matrix based on earlier calced values
+    for row_idx in range(1, num_rows):
         for col_idx in range(1, num_cols):
-            score = alignment_matrix[0][col_idx - 1] + scoring_matrix['-'][seq_y[col_idx - 1]]
-            alignment_matrix[0].insert(col_idx, score)
+            score_pick_both_seq = alignment_matrix[row_idx - 1][col_idx - 1] + scoring_matrix[seq_x[row_idx - 1]][seq_y[col_idx - 1]]
+            score_pick_seq_x = alignment_matrix[row_idx - 1][col_idx] + scoring_matrix[seq_x[row_idx - 1]]['-']
+            score_pick_seq_y = alignment_matrix[row_idx][col_idx - 1] + scoring_matrix['-'][seq_y[col_idx - 1]]
+            score = max(score_pick_both_seq, score_pick_seq_x, score_pick_seq_y)
+            alignment_matrix[row_idx].insert(col_idx, max(score, 0) if not global_flag else score)
 
-        # dynamic programming fill in rest of matrix based on earlier calced values
-        for row_idx in range(1, num_rows):
-            for col_idx in range(1, num_cols):
-                score_pick_both_seq = alignment_matrix[row_idx - 1][col_idx - 1] + scoring_matrix[seq_x[row_idx - 1]][seq_y[col_idx - 1]]
-                score_pick_seq_x = alignment_matrix[row_idx - 1][col_idx] + scoring_matrix[seq_x[row_idx - 1]]['-']
-                score_pick_seq_y = alignment_matrix[row_idx][col_idx - 1] + scoring_matrix['-'][seq_y[col_idx - 1]]
-                score = max(score_pick_both_seq, score_pick_seq_x, score_pick_seq_y)
-                alignment_matrix[row_idx].insert(col_idx, score)
-
-        return alignment_matrix
-    else:
-        # floor score at 0 for local
-        # fill in first row and first col where matching with increasing number of '-'
-        for row_idx in range(1, num_rows):
-            score = alignment_matrix[row_idx - 1][0] + scoring_matrix[seq_x[row_idx - 1]]['-']
-            alignment_matrix.insert(row_idx, [max(score, 0)])
-        for col_idx in range(1, num_cols):
-            score = alignment_matrix[0][col_idx - 1] + scoring_matrix['-'][seq_y[col_idx - 1]]
-            alignment_matrix[0].insert(col_idx, max(score, 0))
-
-        # dynamic programming fill in rest of matrix based on earlier calced values
-        for row_idx in range(1, num_rows):
-            for col_idx in range(1, num_cols):
-                score_pick_both_seq = alignment_matrix[row_idx - 1][col_idx - 1] + scoring_matrix[seq_x[row_idx - 1]][seq_y[col_idx - 1]]
-                score_pick_seq_x = alignment_matrix[row_idx - 1][col_idx] + scoring_matrix[seq_x[row_idx - 1]]['-']
-                score_pick_seq_y = alignment_matrix[row_idx][col_idx - 1] + scoring_matrix['-'][seq_y[col_idx - 1]]
-                score = max(score_pick_both_seq, score_pick_seq_x, score_pick_seq_y)
-                alignment_matrix[row_idx].insert(col_idx, max(score, 0))
-        
-        return alignment_matrix
-        
-
+    return alignment_matrix
+       
 
 print(compute_alignment_matrix('A', 'A', {'A': {'A': 6, 'C': 2, '-': -4, 'T': 2, 'G': 2}, 'C': {'A': 2, 'C': 6, '-': -4, 'T': 2, 'G': 2}, '-': {'A': -4, 'C': -4, '-': -4, 'T': -4, 'G': -4}, 'T': {'A': 2, 'C': 2, '-': -4, 'T': 6, 'G': 2}, 'G': {'A': 2, 'C': 2, '-': -4, 'T': 2, 'G': 6}}, True))
 print(compute_alignment_matrix('A', 'A', {'A': {'A': 6, 'C': 2, '-': -4, 'T': 2, 'G': 2}, 'C': {'A': 2, 'C': 6, '-': -4, 'T': 2, 'G': 2}, '-': {'A': -4, 'C': -4, '-': -4, 'T': -4, 'G': -4}, 'T': {'A': 2, 'C': 2, '-': -4, 'T': 6, 'G': 2}, 'G': {'A': 2, 'C': 2, '-': -4, 'T': 2, 'G': 6}}, False))
