77bdd258c07b82c82977c099352d4a7786b9ed24
leonlinsx <46635657+leonlinsx@users.noreply.github.com>
2024-08-07 22:31:42 -0500

    added local alignment code

--- Rice Fundamentals of Computing specialisation/Algorithmic Thinking/coursera_algo_project_4.py
+++ Rice Fundamentals of Computing specialisation/Algorithmic Thinking/coursera_algo_project_4.py
@@ -1,5 +1,3 @@
-
-
 def build_scoring_matrix(alphabet, diag_score, off_diag_score, dash_score):
     """
     args:
@@ -79,7 +77,116 @@ def compute_alignment_matrix(seq_x, seq_y, scoring_matrix, global_flag):
 
     return alignment_matrix
        
+def compute_global_alignment(seq_x, seq_y, scoring_matrix, alignment_matrix):
+    """
+    args:
+    Takes as input two sequences, seq_x and seq_y, 
+    whose elements share a common alphabet with the scoring matrix scoring_matrix. 
+    
+    This function computes a global alignment of seq_x and seq_y 
+    using the global alignment matrix alignment_matrix. 
+    
+    returns:
+    The function returns a tuple of the form (score, align_x, align_y) 
+    where score is the score of the global alignment, align_x, and align_y. 
+    
+    Note that align_x and align_y should have the same length 
+    and may include the padding character '-'.
+    """
+    row_idx, col_idx = len(seq_x), len(seq_y)
+    align_x, align_y = "", ""
 
-print(compute_alignment_matrix('A', 'A', {'A': {'A': 6, 'C': 2, '-': -4, 'T': 2, 'G': 2}, 'C': {'A': 2, 'C': 6, '-': -4, 'T': 2, 'G': 2}, '-': {'A': -4, 'C': -4, '-': -4, 'T': -4, 'G': -4}, 'T': {'A': 2, 'C': 2, '-': -4, 'T': 6, 'G': 2}, 'G': {'A': 2, 'C': 2, '-': -4, 'T': 2, 'G': 6}}, True))
-print(compute_alignment_matrix('A', 'A', {'A': {'A': 6, 'C': 2, '-': -4, 'T': 2, 'G': 2}, 'C': {'A': 2, 'C': 6, '-': -4, 'T': 2, 'G': 2}, '-': {'A': -4, 'C': -4, '-': -4, 'T': -4, 'G': -4}, 'T': {'A': 2, 'C': 2, '-': -4, 'T': 6, 'G': 2}, 'G': {'A': 2, 'C': 2, '-': -4, 'T': 2, 'G': 6}}, False))
+    # calc scores before row_idx and col_idx are used in while loops
+    if row_idx > 0 and col_idx > 0:
+        score = alignment_matrix[row_idx][col_idx]
+    else:
+        score = 0
+    
+    # traceback walking back in the matrix from entry S[|X|, |Y|] to entry S[0, 0] 
+    # along the path that has score S[|X|, |Y|], 
+    # and while doing so, creating the alignment itself.
+    while row_idx != 0 and col_idx != 0:
+        if alignment_matrix[row_idx][col_idx] == (alignment_matrix[row_idx - 1][col_idx - 1] + scoring_matrix[seq_x[row_idx - 1]][seq_y[col_idx - 1]]):
+            align_x = seq_x[row_idx - 1] + align_x
+            align_y = seq_y[col_idx - 1] + align_y
+            row_idx -= 1
+            col_idx -= 1
+        else:
+            # If the current cell value equals the value from the cell above plus the score for a gap in seq_y, 
+            # a gap ('-') is added to align_y, and the character from seq_x is added to align_x, and row_idx is decremented.
+            if alignment_matrix[row_idx][col_idx] == (alignment_matrix[row_idx - 1][col_idx] + scoring_matrix[seq_x[row_idx - 1]]['-']):
+                align_x = seq_x[row_idx - 1] + align_x
+                align_y = '-' + align_y
+                row_idx -= 1
+            else:
+                align_x = '-' + align_x
+                align_y = seq_y[col_idx - 1] + align_y
+                col_idx -= 1
+    
+    # pad with dash if havent fully transversed rows / cols
+    while row_idx != 0:
+        align_x = seq_x[row_idx - 1] + align_x
+        align_y = '-' + align_y
+        row_idx -= 1
+    while col_idx != 0:
+        align_x = '-' + align_x
+        align_y = seq_y[col_idx - 1] + align_y
+        col_idx -= 1
+
+    return (score, align_x, align_y)
+
+def compute_local_alignment(seq_x, seq_y, scoring_matrix, alignment_matrix):
+    """
+    args:
+    Takes as input two sequences seq_x and seq_y 
+    whose elements share a common alphabet with the scoring matrix scoring_matrix. 
+    
+    This function computes a local alignment of seq_x and seq_y using the local alignment matrix alignment_matrix. 
+    
+    returns:
+    The function returns a tuple of the form (score, align_x, align_y) 
+    where score is the score of the optimal local alignment, align_x, and align_y. 
+    
+    Note that align_x and align_y should have the same length and may include the padding character '-'.
+    """
+    row_idx, col_idx, score = 0, 0, 0
+    align_x, align_y = "", ""
+    
+    for align_row_idx in range(len(seq_x) + 1):
+        for align_col_idx in range(len(seq_y) + 1):
+            if score <= alignment_matrix[align_row_idx][align_col_idx]:
+                score = alignment_matrix[align_row_idx][align_col_idx]
+                row_idx = align_row_idx
+                col_idx = align_col_idx
+
+    # traceback walking back in the matrix from entry S[|X|, |Y|] to entry S[0, 0] 
+    # along the path that has score S[|X|, |Y|], 
+    # and while doing so, creating the alignment itself.
+    # stop the traceback when first entry with value 0 is encountered
+    while row_idx != 0 and col_idx != 0 and alignment_matrix[row_idx][col_idx] != 0:
+        if alignment_matrix[row_idx][col_idx] == (alignment_matrix[row_idx - 1][col_idx - 1] + scoring_matrix[seq_x[row_idx - 1]][seq_y[col_idx - 1]]):
+            align_x = seq_x[row_idx - 1] + align_x
+            align_y = seq_y[col_idx - 1] + align_y
+            row_idx -= 1
+            col_idx -= 1
+        else:
+            # If the current cell value equals the value from the cell above plus the score for a gap in seq_y, 
+            # a gap ('-') is added to align_y, and the character from seq_x is added to align_x, and row_idx is decremented.
+            if alignment_matrix[row_idx][col_idx] == (alignment_matrix[row_idx - 1][col_idx] + scoring_matrix[seq_x[row_idx - 1]]['-']):
+                align_x = seq_x[row_idx - 1] + align_x
+                align_y = '-' + align_y
+                row_idx -= 1
+            else:
+                align_x = '-' + align_x
+                align_y = seq_y[col_idx - 1] + align_y
+                col_idx -= 1
+    
+    # do not need to pad with dash if havent fully transversed rows / cols, since getting local alignment only
+
+    return (score, align_x, align_y)  
+
+
 # print(build_scoring_matrix(set(['A', 'C', 'T', 'G']), 6, 2, -4))
+# print(compute_alignment_matrix('A', 'A', {'A': {'A': 6, 'C': 2, '-': -4, 'T': 2, 'G': 2}, 'C': {'A': 2, 'C': 6, '-': -4, 'T': 2, 'G': 2}, '-': {'A': -4, 'C': -4, '-': -4, 'T': -4, 'G': -4}, 'T': {'A': 2, 'C': 2, '-': -4, 'T': 6, 'G': 2}, 'G': {'A': 2, 'C': 2, '-': -4, 'T': 2, 'G': 6}}, True))
+# print(compute_alignment_matrix('A', 'A', {'A': {'A': 6, 'C': 2, '-': -4, 'T': 2, 'G': 2}, 'C': {'A': 2, 'C': 6, '-': -4, 'T': 2, 'G': 2}, '-': {'A': -4, 'C': -4, '-': -4, 'T': -4, 'G': -4}, 'T': {'A': 2, 'C': 2, '-': -4, 'T': 6, 'G': 2}, 'G': {'A': 2, 'C': 2, '-': -4, 'T': 2, 'G': 6}}, False))
+# print(compute_global_alignment('A', 'A', {'A': {'A': 6, 'C': 2, '-': -4, 'T': 2, 'G': 2}, 'C': {'A': 2, 'C': 6, '-': -4, 'T': 2, 'G': 2}, '-': {'A': -4, 'C': -4, '-': -4, 'T': -4, 'G': -4}, 'T': {'A': 2, 'C': 2, '-': -4, 'T': 6, 'G': 2}, 'G': {'A': 2, 'C': 2, '-': -4, 'T': 2, 'G': 6}}, [[0, -4], [-4, 6]]))
